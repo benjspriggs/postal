ae9e909d3c676e0d7eb0e8dd67903d19b9d75493
bspriggs <ben@sprico.com>
2018-07-31 14:14:33 -0700

    Import from csv

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import sys
 from geolocation.main import GoogleMaps
-
-address = "New York City Wall Street 12"
+import pandas as pd
 
 google_maps = GoogleMaps(api_key='your_google_maps_key') 
 
-location = google_maps.search(location=address) # sends search to Google Maps.
 
-print(location.all()) # returns all locations.
+def get_location_model(address):
+    print('Searching address: ', address)
+    return google_maps.search(location=address)
+
+key = 'Mailing Address'
+
+def main():
+    guest_list = pd.read_csv(sys.argv[1])
+    existing = guest_list[key].notnull() 
+    mailing_addresses = guest_list[existing]
+    locations = map(lambda row: get_location_model(row[1][key]), mailing_addresses.iterrows())
+    print(list(locations))
+
+if __name__ == "__main__":
+    main()
